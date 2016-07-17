542a4274c4cc01651ac41338807d025f032fe0d7
JOSEGM96 <jose_g_m.96@hotmail.com>
2016-07-17 16:07:08 -0400

    Add files via upload

---
+++ XP/Modelo/Tests/TareaTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jose
+ */
+public class TareaTest {
+    
+    public TareaTest() {
+    }        
+    @Before
+    public void setUp()
+    {
+    }
+    @Test
+    public void testDescripcionTarea(){
+        Tarea tarea=new Tarea();
+        tarea.setDescripcion("Prueba de Tarea");
+        assertEquals(tarea.getDescripcion(),"Prueba de Tarea");
+    }
+    @Test
+    public void testImportancia(){
+        Tarea tarea=new Tarea();        
+        tarea.setImportancia(5);
+        assertEquals(5,tarea.getImportancia());
+    }
+    @Test
+    public void testTareaAsignada(){ 
+        Tarea tarea=new Tarea(); 
+        assertFalse(tarea.estaAsignada());        
+        tarea.asignar();
+        assertTrue(tarea.estaAsignada());
+    }
+    @Test
+    public void testTareaFinalizada(){        
+        Tarea tarea=new Tarea();         
+        assertFalse(tarea.estaFinalizada());       
+        tarea.finalizar();            
+        assertTrue(tarea.estaFinalizada());
+    }
+    @Test
+    public void testTareaRechazada(){
+        Tarea tarea=new Tarea();         
+        assertFalse(tarea.estaRechazada());        
+        tarea.rechazar();        
+        assertTrue(tarea.estaRechazada());
+    }
+    @Test
+    public void testHistoriaAprobada(){
+        Tarea tarea=new Tarea();         
+        assertFalse(tarea.estaAprobada());        
+        tarea.aprobar();        
+        assertTrue(tarea.estaAprobada());
+    }    
+    @After
+    public void tearDown() {
+    } 
+    
+}
