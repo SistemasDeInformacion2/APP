306ff8c80d7044428261b2f472be40a8e798fc05
cesarquirogac <cesar.a.quiroga.c+github@gmail.com>
2016-07-19 02:42:17 -0400

    Update PuenteDB.java

--- XP/Control/PuenteDB.java
+++ XP/Control/PuenteDB.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class PuenteDB 
 {
     private String driver = "org.postgresql.Driver";
-    private String url = "jdbc:postgresql://10.0.0.27:5432/DBSis";
+    private String url = "jdbc:postgresql://localhost:5432/DBSis";
     private String username = "postgres";
     private String pass = "admin";
     protected Connection connection;
